4ac61a1eeb6a3aaa73943ced2b98cc4d43a4bd5a
olcay <olcaymaster@gmail.com>
2022-03-04 02:40:22 +0300

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movie-app-01';
+
+  items = [
+    {
+      title: 'Dunkirk',
+      tags: ['Bitski', 'Multi', 'Sold'],
+      url: 'https://blutv-images.mncdn.com/q/t/i/bluv2/75/434x231/5e96bf1d866ac31a8856ccd1?w=720&q=75'
+    },
+    {
+      title: 'Love & Other Drugs',
+      tags: ['$MEME', 'Multi'],
+      url: 'https://blutv-images.mncdn.com/q/t/i/bluv2/75/434x231/603cbf5c866ac31798b3f1ac?w=434&q=75'
+    },
+    {
+      title: 'A Perfect Day',
+      tags: ['SuperRare', 'Single', 'Secondary'],
+      url: 'https://blutv-images.mncdn.com/q/t/i/bluv2/75/434x231/6049df69866ac31fcc5d00c1?w=434&q=75'
+    },
+    {
+      title: 'Lady Macbeth',
+      tags: ['OpenSea', 'Single', 'Sold', 'Bespoke'],
+      url: 'https://blutv-images.mncdn.com/q/t/i/bluv2/75/434x231/619247b1866ac31084da57c2?w=434&q=75'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
